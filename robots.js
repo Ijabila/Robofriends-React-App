2446a98b8bd37d656b616db0e148c2ec6674ebe7
Ijabila <44542779+Ijabila@users.noreply.github.com>
2019-02-04 17:19:02 +0100

    Add files via upload

---
+++ robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Ajirireloja Olubambam',
+    username: 'Em',
+    email: 'shortmandevil@hell.com'
+  },
+  {
+    id: 2,
+    name: 'Fart Smoker',
+    username: 'Arinze',
+    email: 'schomoker@farts.cigar'
+  },
+  {
+    id: 3,
+    name: 'Fat Cow',
+    username: 'I\'m sorry guys',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Dan Vic',
+    username: 'Bororo',
+    email: 'ikeinfinix@knack.org'
+  },
+  {
+    id: 5,
+    name: 'Bukoye Enoch',
+    username: 'Longitude',
+    email: 'youarenot@enoch.bible'
+  },
+  {
+    id: 6,
+    name: 'John Paul',
+    username: 'Arsenal',
+    email: 'nEwErAaRsEnAl@NoBaNtEr.6th'
+  },
+  {
+    id: 7,
+    name: 'Eluma Sataniah',
+    username: 'Lucifer',
+    email: 'hellfire@2023.hot'
+  },
+  {
+    id: 8,
+    name: 'I am Tired',
+    username: 'Life no just easy',
+    email: 'wizkidfc@starboy.com'
+  },
+  {
+    id: 9,
+    name: 'Anthony Martial',
+    username: 'Is better than Hazard',
+    email: 'nextronaldo@united.com'
+  },
+  {
+    id: 10,
+    name: 'Meat Beater',
+    username: 'Slap the Salame',
+    email: 'nivesfc@night.com'
+  }
+];
