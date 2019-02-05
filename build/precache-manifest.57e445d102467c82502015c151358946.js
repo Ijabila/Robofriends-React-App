2915eb9d1d35b7872a17d204052848ca51cb2896
Ijabila <44542779+Ijabila@users.noreply.github.com>
2019-02-05 11:35:24 +0100

    Build files for the app

---
+++ build/precache-manifest.57e445d102467c82502015c151358946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ed22fa8d50d76ffb760d",
+    "url": "/static/js/main.ed22fa8d.chunk.js"
+  },
+  {
+    "revision": "c68a0d02abafb9f2c236",
+    "url": "/static/js/1.c68a0d02.chunk.js"
+  },
+  {
+    "revision": "ed22fa8d50d76ffb760d",
+    "url": "/static/css/main.c3ab2bd0.chunk.css"
+  },
+  {
+    "revision": "c68a0d02abafb9f2c236",
+    "url": "/static/css/1.dc32ce5f.chunk.css"
+  },
+  {
+    "revision": "d84ed83e16db8c194f83a7eea6cfe0ce",
+    "url": "/index.html"
+  }
+];
